0a095078c61c39f9d4a9573434f00dbee0624711
Angie Tatiana Malca Díaz <amalcad22_2@unc.edu.pe>
2026-02-05 21:38:04 -0500

    Update prueba_app_techy.py

--- prueba_app_techy.py
+++ prueba_app_techy.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
 st.title("Hola estoy creando mi app web)
-         
+st.caption("He creado mi página web en el workshop techy.")
+
+st.code("""
+import pandas as pd
+""")
+
